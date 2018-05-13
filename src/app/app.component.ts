bd6c7482b88dcea5b3dd4c7c7f0276a1b540bfc9
Ben Stiefel <ben.stiefel@bytecubed.com>
2018-05-12 22:42:31 -0700

    use pan, not swipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,8 @@ import * as kf from './keyframes';
   animations: [
     trigger('flyInOut', [
       state('in', style({transform: 'translateX(0)'})),
-      transition('void => *', [
-        style({transform: 'translateX(-100%)'}),
-        animate(100)
-      ]),
       transition('* => void', [
-        animate(100, style({transform: 'translateX(100%)'}))
+        animate('350ms ease-out', style({transform: 'translateX(100%)'})),
       ])
     ])
   ]
